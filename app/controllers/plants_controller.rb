4b86e03b14c6b86964dbfe268e91d603f2cb2737
youbaidjis <youba.etud@gmail.com>
2022-08-21 22:29:49 -0700

    commit tree 2c97bed9d210646287a644162adfc8bf49220b6b

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #PATCH /plants/id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: { error: "not found" }, status: :not_found
+    end
+  end
+
+  #DELETE /plants/id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "not found" }, status: :not_found
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
+
